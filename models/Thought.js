0ac4b44ff595c2dc50cd51dfb625616394e7e229
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-02-27 21:05:21 -0500

    finished models and added routes files

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,38 @@
-const { Schema, Types } = require('mongoose');
+const { Schema } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText:{
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => timeSince(date),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [ reactionSchema]
+      
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
+  thoughtSchema.virtual('rectionCount').get(function () {
+    return this.reactions.length;
+  });
+  const Thought = model('Thought', thoughtSchema);
+
+  module.exports = Thought;
