8c6611136eac1820e36a62ac126807b65515816e
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-02-27 20:39:06 -0500

    added folders and added User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, Types } = require('mongoose');
+const validatorPackage = require('validator')
+
+const userSchema = new Schema(
+    {
+      username:{
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: validatorPackage.isEmail,
+            message: 'Please provide a valid email',
+          },
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        getters: true,
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+  
+  postSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+  const User = model('User', userSchema);
+
+module.exports = User;
