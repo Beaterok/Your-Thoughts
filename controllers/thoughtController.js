1caf50430ef817f995c90b3685457832144a16d9
Alex Loburev <48661813+Beaterok@users.noreply.github.com>
2023-02-28 23:26:01 -0500

    final touch

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,8 +4,8 @@ module.exports={
 
     getThoughts(req, res) {
         Thought.find()
-          .then((thoughts) => res.json(thoughts))
-          .catch((err) => res.status(500).json(err));
+          .then((thought) => res.json(thought))
+          .catch((err) => res.status(500).json(err))
       },
       getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
